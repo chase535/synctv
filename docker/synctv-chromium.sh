26f663f408f37afc06805d4eded12558b388ee43
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2026-09-02 16:43:07 +0800

    perf: load only the iQiyi bootstrap probe extension

---
+++ docker/synctv-chromium.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -eu
+
+EXTENSION_DIR=/usr/local/share/synctv-iqiyi-bootstrap
+
+filter_and_exec() {
+  for original do
+    shift
+    if [ "$original" = "--disable-extensions" ]; then
+      continue
+    fi
+    set -- "$@" "$original"
+  done
+
+  exec /usr/bin/chromium \
+    --autoplay-policy=no-user-gesture-required \
+    --window-size=1280,720 \
+    --disable-background-timer-throttling \
+    --disable-backgrounding-occluded-windows \
+    --disable-renderer-backgrounding \
+    --disable-blink-features=AutomationControlled \
+    --disable-extensions-except="$EXTENSION_DIR" \
+    --load-extension="$EXTENSION_DIR" \
+    "$@"
+}
+
+filter_and_exec "$@"
